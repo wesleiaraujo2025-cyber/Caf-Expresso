be2e82449677c0ca4e3f2bb200d44f575081aa80
wesleiaraujo2025-cyber <wesleiaraujo2025@gmail.com>
2026-04-21 23:52:36 -0300

    Add Produto class with constructor and display method

--- src/Produto.java
+++ src/Produto.java
@@ -1,1 +1,14 @@
 
+public class Produto {
+    String nome;
+    double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public void mostrar() {
+        System.out.println("Produto: " + nome + " - R$ " + preco);
+    }
+}
